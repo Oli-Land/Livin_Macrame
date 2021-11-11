c8ffb69e62a5b782a0f32834b249f0d6489fdc6d
OliLand <oli.landreth@gmail.com>
2021-11-11 17:35:42 +1100

    added templates to all other routes

--- FlaskApp/controllers/course_controller.py
+++ FlaskApp/controllers/course_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, request, render_template, redirect, url_for
 from main import db
 from models.courses import Course
 from schemas.course_schema import courses_schema, course_schema
@@ -7,19 +7,15 @@ from schemas.course_schema import courses_schema, course_schema
 courses = Blueprint('courses', __name__)
 
 
+### VIEWS ###
+
+# Homepage route endpoint
 @courses.route('/')
-def hello_world():
-    return "Hello world!"
-
-@courses.route('/students/')
-def get_students():
-    return "This will be a list of students "
-
-@courses.route('/students/<int:student_id>/')
-def get_specific_students(student_id):
-    return f"This will be a page displaying info about {student_id} "
-
-
+def home_page():
+    data = {
+    "page_title": "Homepage"
+    }
+    return render_template("homepage.html", page_data=data)
 
 
 # The GET routes endpoint
@@ -29,33 +25,48 @@ def get_courses():
     "page_title": "Course Index",
     "courses": courses_schema.dump(Course.query.all())
     }
-    return render_template("course_index.html", page_data = data)
+    return render_template("course_index.html", page_data=data)
 
+# The POST routes endpoint
 @courses.route("/courses/", methods=["POST"])
 def create_course():
     new_course = course_schema.load(request.form)
     db.session.add(new_course)
     db.session.commit()
-    return jsonify(course_schema.dump(new_course))
+    return redirect(url_for("courses.get_courses")) 
 
-@courses.route("/courses/<int:id>/", methods = ["GET"])
+# The GET specific route endpoint
+@courses.route("/courses/<int:id>/", methods=["GET"])
 def get_course(id):
     course = Course.query.get_or_404(id)
-    return jsonify(course_schema.dump(course))
+    data = {
+        "page_title": "Course Detail",
+        "course": course_schema.dump(course)
+    }
+    return render_template("course_detail.html", page_data=data)
 
-@courses.route("/courses/<int:id>/", methods=["PUT", "PATCH"])
+# The PUT/PATCH route
+@courses.route("/courses/<int:id>/", methods=["POST"])
 def update_course(id):
+
     course = Course.query.filter_by(course_id=id)
-    updated_fields = course_schema.dump(request.json)
+
+    updated_fields = course_schema.dump(request.form)
     if updated_fields:
         course.update(updated_fields)
         db.session.commit()
-    return jsonify(course_schema.dump(course.first()))
+    
+    data = {
+        "page_title": "Course Detail",
+        "course": course_schema.dump(course.first())
+    }
+    return render_template("course_detail.html", page_data=data)
 
-@courses.route("/courses/<int:id>/", methods=["DELETE"])
+# The DELETE endpoint
+@courses.route("/courses/<int:id>/delete/", methods=["POST"])
 def delete_course(id):
     course = Course.query.get_or_404(id)
     db.session.delete(course)
     db.session.commit()
-    return jsonify(course_schema.dump(course))
+    return redirect(url_for("courses.get_courses"))
 
