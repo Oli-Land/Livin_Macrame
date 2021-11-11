5ac99298f1cba5ecb007bebeb18ef5fe3b8ce069
OliLand <oli.landreth@gmail.com>
2021-11-11 11:49:48 +1100

    using templates successfully

--- FlaskApp/tests/test_courses.py
+++ FlaskApp/tests/test_courses.py
@@ -24,3 +24,26 @@ class TestCourses(unittest.TestCase):
     def test_create_bad_course(self):
         response = self.client.post("/courses/", json={"course_name": ""})
         self.assertEqual(response.status_code, 400)
+
+    def test_create_good_course(self):
+        response = self.client.post("/courses/", json={"course_name": "testcourse"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()["course_name"], "testcourse")
+        self.client.delete(f"/courses/{response.get_json()['course_id']}/")
+
+    def test_delete_course(self):
+        response1 = self.client.post("/courses/", json={"course_name": "testcourse"})
+        id = response1.get_json()["course_id"]
+        
+        response2 = self.client.delete(f"/courses/{id}/")
+        self.assertEqual(response2.status_code, 200)
+
+    def test_update_course(self):
+        response1 = self.client.post("/courses/", json={"course_name": "testcourse"})
+        id = response1.get_json()["course_id"]
+        
+        response2 = self.client.put(f"/courses/{id}/", json={"course_name": "newtestcourse"})
+        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response2.get_json()["course_name"], "newtestcourse")
+        
+        self.client.delete(f"/courses/{id}/")
